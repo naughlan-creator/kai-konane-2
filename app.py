d00eab041fcd8fe7f9a8bb3c27dc971c1535a02d
naughlan-creator <naughlantemp@gmail.com>
2024-10-21 19:23:39 +0200

    Added create admin logic

--- app.py
+++ app.py
@@ -8,20 +8,21 @@ import os
 
 @app.cli.command("create-admin")
 def create_admin():
-    existing_admin = User.query.filter_by(username='admin').first()
-    if existing_admin:
-        print("Admin user already exists.")
-    else:
-        admin = Admin(
-            username='admin',
-            password=generate_password_hash(os.environ.get('ADMIN_PASSWORD', '1234')),
-            email=os.environ.get('ADMIN_EMAIL', 'admin@example.com'),
-            name='Admin',
-            role=Role.ADMIN
-        )
-        db.session.add(admin)
-        db.session.commit()
-        print("Admin user created successfully.")
+    with app.app_context():
+        existing_admin = User.query.filter_by(username='admin').first()
+        if existing_admin:
+            print("Admin user already exists.")
+        else:
+            admin = Admin(
+                username='admin',
+                password=generate_password_hash(os.environ.get('ADMIN_PASSWORD', '1234')),
+                email=os.environ.get('ADMIN_EMAIL', 'admin@example.com'),
+                name='Admin',
+                role=Role.ADMIN
+            )
+            db.session.add(admin)
+            db.session.commit()
+            print("Admin user created successfully.")
 
 @app.cli.command("init-db")
 def init_db():
@@ -51,4 +52,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    create_admin()
     app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from app import app, create_admin
+
+create_admin()
+
+if __name__ == "__main__":
+    app.run()
