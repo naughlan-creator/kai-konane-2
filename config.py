a59f9a4ce33d0727194bcc4d8e5667b4718caa13
naughlan-creator <naughlantemp@gmail.com>
2024-10-22 16:20:52 +0200

    switched to support postgresql - change DB_URL

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('JAWSDB_URL', 'postgresql://naugh:1234@localhost:5432/kai_konane')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://naugh:1234@localhost:5432/kai_konane')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '1234')
 
