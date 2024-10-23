18b7ee1376b3b4da02ea53a34f00e3e3aab4a8e7
naughlan-creator <naughlantemp@gmail.com>
2024-10-23 08:45:43 +0200

    ready deployment for heroku

--- app.py
+++ app.py
@@ -52,7 +52,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    init_db()
-    create_admin()
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
