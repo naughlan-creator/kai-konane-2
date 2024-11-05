13f4483d9e09a18fee131614b69b9af2e0e4a2e3
naughlan-creator <naughlantemp@gmail.com>
2024-11-05 09:55:30 +0200

    updated database for render

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('JAWSDB_URL', 'mysql://naugh:1234@localhost:3306/kai_konane')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('JAWSDB_URL', 'postgresql://kai_konane_user:Yixi8qRycyKnKKZ17uOqBL5Nzx0zZ2Ft@dpg-csbrcl5ds78s73be6bk0-a.oregon-postgres.render.com/kai_konane')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '1234')
 
